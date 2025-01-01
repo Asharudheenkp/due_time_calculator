97913a02c85fa29512723e1e2d100cb0c61a359e
Asharudheenkp <asharudheenkpi786@gmail.com>
2025-01-01 19:16:47 +0530

    show the new year popup only in the january 1

--- app/page.tsx
+++ app/page.tsx
@@ -9,10 +9,15 @@ const Interactive3d = dynamic(() => import('../components/Interactive3d'), {
   ssr: false 
 });
 export default function Home() {
+  const isNewYear = () => {
+    const today = new Date();
+    return today.getMonth() === 0 && today.getDate() === 1;
+  };
+
   return (
     <div className="mt-9 w-[80%] mx-auto">
       <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
-        <NewYearPopup/>
+      {isNewYear() && <NewYearPopup />} 
         <Calculator/>
         <Interactive3d />
         <AdviceOfTheSecond/>
