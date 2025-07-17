327b04a3c693770e94ce622dd77e9cf94672492f
asharudheenkp <asharudheenkpi786@gmail.com>
2025-07-17 23:02:16 +0530

    added advice json

--- app/api/advice/route.tsx
+++ app/api/advice/route.tsx
@@ -1,21 +1,38 @@
 import { NextResponse } from "next/server";
 import connect from "@/lib/db";
-import Advices from '@/lib/models/advices'
+// import Advices from '@/lib/models/advices'
+import Advices from '@/lib/advices_365.json'
 
+
+
+// export async function GET() {
+//   try {
+//     await connect();
+//     const randomAdvice = await Advices.aggregate([
+//       { $sample: { size: 1 } }
+//     ]);
+
+//     if (!randomAdvice || randomAdvice.length === 0) {
+//       return new NextResponse("No advice found", { status: 404 });
+//     }
+
+//     return new NextResponse(JSON.stringify(randomAdvice[0]));
+//   } catch (error: any) {
+//     return new NextResponse("Error in fetching advice: " + error.message, { status: 500 });
+//   }
+// }
 
 export async function GET() {
   try {
-    await connect();
-    const randomAdvice = await Advices.aggregate([
-      { $sample: { size: 1 } }
-    ]);
+    const randomIndex = Math.floor(Math.random() * Advices.length);
+    const randomAdvice = Advices[randomIndex];
 
-    if (!randomAdvice || randomAdvice.length === 0) {
-      return new NextResponse("No advice found", { status: 404 });
-    }
-
-    return new NextResponse(JSON.stringify(randomAdvice[0]));
+    return new NextResponse(JSON.stringify(randomAdvice), {
+      headers: { "Content-Type": "application/json" }
+    });
   } catch (error: any) {
-    return new NextResponse("Error in fetching advice: " + error.message, { status: 500 });
+    return new NextResponse("Error in fetching advice: " + error.message, {
+      status: 500
+    });
   }
 }
